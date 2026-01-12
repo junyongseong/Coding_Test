b01378fcd53bd4b908154c6c0959754aa7b5d4b5
성용준 <43407026+junyongseong@users.noreply.github.com>
2026-01-12 22:24:50 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 96 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        //이게맞아?ㅋㅋ
+        System.out.println("A");
+    }
+}
